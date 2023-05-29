c1b0121234caae2bbc0777f206e25f4438cfbcac
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-05-29 18:00:46 +0200

    added discord.js file

---
+++ discord.js
@@ -0,0 +1,11 @@
+const { Client, IntentsBitField } = require('discord.js')
+const internal = require('stream')
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+})
