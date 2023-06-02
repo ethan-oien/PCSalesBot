92276689febb280b8332ac940b85dd93b4a96612
Ethan Oien <oien.ethan.c@gmail.com>
2023-06-01 21:07:18 -0400

    Changed double quotes to single quotes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const { getAccessToken } = require("./reddit/auth")
+const { getAccessToken } = require('./reddit/auth')
 
 const app = express();
 
--- reddit/auth.js
+++ reddit/auth.js
@@ -3,15 +3,15 @@ const axios = require('axios').default;
 async function getAccessToken()
 {
     return new Promise((resolve, reject) => { 
-        const url = "https://www.reddit.com/api/v1/access_token";
+        const url = 'https://www.reddit.com/api/v1/access_token';
 
         const data = {
-            grant_type: "client_credentials"
+            grant_type: 'client_credentials'
         }
 
         const headers = {
-            "Authorization": "Basic " + Buffer.from(`${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_SECRET}`).toString('base64'),
-            "Content-Type": "application/x-www-form-urlencoded"
+            'Authorization': 'Basic ' + Buffer.from(`${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_SECRET}`).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
 
         axios.post(url, data, {
